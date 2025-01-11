f6e1f72ce9ae1852a4d376a6cab20f20f4cfcd5b
Tosmim <tosmimforidmehtab@gmail.com>
2025-01-11 11:24:11 +0530

    Added wallet and drawer

---
+++ src/components/Profile.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { useAuth } from "../contexts/AuthContext";
+import { jwtDecode } from "jwt-decode";
+
+const Profile = () => {
+	const { user } = useAuth();
+	console.log(user);
+	return (
+		<div className="max-w-2xl mx-auto mt-8">
+			<h2 className="text-2xl font-bold mb-4 text-center uppercase">
+				Profile
+			</h2>
+			<div className="bg-white shadow-md rounded-lg p-6 text-black">
+				<p className="text-lg">
+					<span className="font-semibold">Email:</span> {user.email}
+				</p>
+				{/* Add more user information here */}
+			</div>
+		</div>
+	);
+};
+
+export default Profile;
--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,27 +1,26 @@
-import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { useAuth } from '../contexts/AuthContext';
-import { ClipLoader } from 'react-spinners';
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useAuth } from "../contexts/AuthContext";
+import { ClipLoader } from "react-spinners";
 
 const SignIn = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const navigate = useNavigate();
-  const { login, loading } = useAuth();
-  const [showPassword, setShowPassword] = useState(false);
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
+	const navigate = useNavigate();
+	const { login, loading } = useAuth();
+	const [showPassword, setShowPassword] = useState(false);
 
-  const handleSubmit = async (e) => {
-   try {
-    e.preventDefault();
-    const response = await login(email, password);
-    if(response)
-    navigate('/');
-   } catch (error) {
-    console.error(error);
-   }
-  };
+	const handleSubmit = async (e) => {
+		try {
+			e.preventDefault();
+			const response = await login(email, password);
+			if (response) navigate("/");
+		} catch (error) {
+			console.error(error);
+		}
+	};
 
-  return (
+	return (
 		<div className=" flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
 			<div className="max-w-md w-full space-y-8">
 				<div>
@@ -61,7 +60,9 @@ const SignIn = () => {
 									className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
 									placeholder="Password"
 									value={password}
-									onChange={(e) => setPassword(e.target.value)}
+									onChange={(e) =>
+										setPassword(e.target.value)
+									}
 								/>
 
 								<div className="bg-indigo-600 z-10 text-white ml-2 absolute right-0 rounded-md p-[6px] rounded-tr-none">
@@ -77,7 +78,6 @@ const SignIn = () => {
 								</div>
 							</div>
 						</div>
-						
 					</div>
 					<div>
 						<button
@@ -85,14 +85,17 @@ const SignIn = () => {
 							disabled={loading}
 							className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
 						>
-							{loading ? <ClipLoader color="#ffffff" size={20} /> : "Sign in"}
+							{loading ? (
+								<ClipLoader color="#ffffff" size={20} />
+							) : (
+								"Sign in"
+							)}
 						</button>
 					</div>
 				</form>
 			</div>
 		</div>
-  );
+	);
 };
 
 export default SignIn;
-
